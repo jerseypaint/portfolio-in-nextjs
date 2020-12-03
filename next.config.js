d1e3773c438517f1d47605d0d9999451d89c047b
Aaron <jerseypaint@gmail.com>
2020-12-03 17:53:36 -0500

    config fix for vercel deploy

--- next.config.js
+++ next.config.js
@@ -2,4 +2,9 @@ module.exports = {
     images: {
       domains: ['images.ctfassets.net'],
     },
+    env: {
+        CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+        CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+        CONTENTFUL_PREVIEW_ACCESS_TOKEN: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+      },
   }
